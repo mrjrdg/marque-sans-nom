3e283ea88c1e3f32e0009611984d3fb865d68997
Philippe Soucy <phsoucy@gmail.com>
2020-02-25 23:47:36 -0500

    Intégration de la page d'accueil et retouche des pages de connexion

--- webapp/ViewModels/RegisterViewModel.cs
+++ webapp/ViewModels/RegisterViewModel.cs
@@ -7,26 +7,28 @@ namespace ViewModels
     public class RegisterViewModel
     {
         [Required]
-        [Display(Name = "Firstname")]
+        [Display(Name = "Prénom")]
         public string FirstName { get; set; }
 
         [Required]
-        [Display(Name = "Lastname")]
+        [Display(Name = "Nom de famille")]
         public string LastName { get; set; }
 
         [Required]
+        [Display(Name = "Courriel")]
         [EmailAddress]
         [Remote(action: "IsEmailInUse", controller: "Account")]
         public string Email { get; set; }
 
         [Required]
+        [Display(Name = "Mot de famille")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
+        [Display(Name = "Confirmer son mot de passe")]
         [Compare("Password",
-            ErrorMessage = "Password and confirmation password do not match.")]
+            ErrorMessage = "Les deux mots de passe ne correspondent pas")]
         public string ConfirmPassword { get; set; }
     }
 }
