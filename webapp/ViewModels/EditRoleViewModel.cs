1bcf2cce0589cfe00bf782b521e74789aa58be3b
Alex <alexandre.bourdeau@mailbox.org>
2020-04-05 15:44:50 -0400

    Fixed issue #37

--- webapp/ViewModels/EditRoleViewModel.cs
+++ webapp/ViewModels/EditRoleViewModel.cs
@@ -6,9 +6,15 @@ namespace ViewModels
     public class EditRoleViewModel
     {
         public string Id { get; set; }
-        [Required(ErrorMessage = "Role name is required")]
+
+        [Display(Name = "Nom du rôle")]
+        [Required(ErrorMessage = "Le nom du rôle est requis.")]
+        [RegularExpression("^.{1,16}$", ErrorMessage = "Veuillez saisir un maximum de 16 caractères dans le nom du rôle.")]
         public string RoleName { get; set; }
+
+        [Display(Name = "Utilisateurs associés au rôle")]
         public List<string> Users { get; set; }
+
 
         public EditRoleViewModel()
         {
